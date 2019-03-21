1f0fdd6808d2501afbfc99d704cc8f1e73430851
Alberto <eks.alberto@gmail.com>
2019-03-21 21:50:41 +1100

    add sortByOptions method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component{
+
+}
+
+renderSortByOptions(){
+  return Object.Keys(sortByOptions).map(sortByOption => {
+
+  }););
+}
