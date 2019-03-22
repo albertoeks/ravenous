c6f81cc195819332fa98e9b4cc1693912ad0b34e
Alberto <eks.alberto@gmail.com>
2019-03-22 23:47:23 +1100

    adjustes in renderSortByOptions

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,11 +8,36 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component{
+  render() {
+    return(
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Lets Go</a>
+        </div>
+      </div>
+    );
+  }
 
+
+  renderSortByOptions() {
+    return Object.Keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return (
+        <li className={sortByOptionValue} key={sortByOptionValue}> {sortByOption}
+        </li>
+      );
+    });
+
+  }
 }
 
-renderSortByOptions(){
-  return Object.Keys(sortByOptions).map(sortByOption => {
-
-  }););
-}
+export default SearchBar;
