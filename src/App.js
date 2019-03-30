478dc0f374b6bf9bb9db51cd213bd589c6b5118b
Alberto <eks.alberto@gmail.com>
2019-03-30 20:28:54 +1100

    moved business information

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
+
+var business = {
+
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'Lucca Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+  };
+
+  const businesses = [business, business, business, business, business, business];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar/>
-        <BusinessList/>
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
