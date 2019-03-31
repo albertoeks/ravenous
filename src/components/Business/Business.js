fd185fc1838985546b10adc122befb3c525dd750
Alberto <eks.alberto@gmail.com>
2019-03-31 23:17:12 +1100

    fix format component Business

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,25 +6,26 @@ class Business extends React.Component {
   render() {
     const {business} = this.props;
     return(
-    <div className="Business">
-      <div className="image-container">
-        <img src={business.imageSrc} alt=''/>
-      </div>
-      <h2>{business.name}</h2>
-      <div className="Business-information">
-        <div className="Business-address">
-          <p>{business.address}</p>
-          <p>{business.city}</p>
-          <p>{business.state} {business.zipCode}</p>
+      <div className="Business">
+        <div className="image-container">
+          <img src={business.imageSrc} alt=''/>
         </div>
-        <div className="Business-reviews">
-          <h3>{business.category}</h3>
-          <h3 className="rating">{business.rating} stars</h3>
-          <p>{business.reviewCount} reviews</p>
+        <h2>{business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.state}</p> 
+            <p>{business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{business.category}</h3>
+            <h3 className="rating">{business.rating} stars</h3>
+            <p>{business.reviewCount} reviews</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 
