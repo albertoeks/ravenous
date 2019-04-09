f9ab6dc74b894634668bc701a84bb1b335af973d
Alberto <eks.alberto@gmail.com>
2019-04-09 22:41:01 +1000

    Create access to Yelp API

---
+++ src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = _AKCpWTPCcjevRc-BY1Uqbk5wR-Zn-X7N3TTbCsuWyh6drpUHOxvEM5hn06s7ADvtzeaRDIQheYIZn0tFpxlxpK_wXehSQhI-Dw7JH0mjPlKlS6nIjlegyiQQ3asXHYx;
+
+const Yelp;
+
+search(term, location, sortBy){
+    return fetch();
+}
