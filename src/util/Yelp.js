6fb4642455432d8bb0ab4fb6fd2dd2a3829c07e4
Alberto <eks.alberto@gmail.com>
2019-04-10 23:01:52 +1000

    fix search method yelp

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,5 +3,10 @@ const apiKey = _AKCpWTPCcjevRc-BY1Uqbk5wR-Zn-X7N3TTbCsuWyh6drpUHOxvEM5hn06s7ADvt
 const Yelp;
 
 search(term, location, sortBy){
-    return fetch();
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,  { 
+        headers: {
+          Authorization: `Bearer ${apiKey}`} 
+        }).then(response => {
+            return response.json();
+          });
 }
